95d2cbbb74db2fff246a22e2b9e9f8c271039d5a
tanikawat <tuliot@grad.ufsc.br>
2018-01-18 12:28:36 -0200

    Primeiro Commit

---
+++ api/routes/taskRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var tasklList = require('../controllers/tasksController');
+
+  // todoList Routes
+  app.route('/tasks')
+    .get(tasklList.list_all_tasks)
+    .post(tasklList.create_a_task);
+
+
+  app.route('/tasks/:taskId')
+    .get(tasklList.read_a_task)
+    .put(tasklList.update_a_task)
+    .delete(tasklList.delete_a_task);
+};
